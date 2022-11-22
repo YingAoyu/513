10287145a116c73ee8d616c9209f01fe96a26b9c
YingAoyu <103395208+YingAoyu@users.noreply.github.com>
2022-11-22 09:37:06 +0800

    Add files via upload

---
+++ register.php
@@ -0,0 +1,409 @@
+
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$name = $address = $salary = $username = $password = $confirm_password = "";
+$name_err = $address_err = $salary_err = $username_err = $password_err = $confirm_password_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+// Validate name
+if(empty(trim($_POST["name"]))){
+    $name_err = "Please enter a name.";     
+} elseif(strlen(trim($_POST["name"])) < 6){
+    $name_err = "name must have atleast 6 characters.";
+} else{
+    $name = trim($_POST["name"]);
+}
+
+// Validate address
+if(empty(trim($_POST["address"]))){
+    $address_err = "Please enter a address.";     
+} elseif(strlen(trim($_POST["address"])) < 6){
+    $address_err = "address must have atleast 6 characters.";
+} else{
+    $address = trim($_POST["address"]);
+}
+
+// Validate salary
+if(empty(trim($_POST["salary"]))){
+    $salary_err = "Please enter a salary.";     
+} elseif(strlen(trim($_POST["salary"])) < 0){
+    $salary_err = "salary must have atleast 6 characters.";
+} else{
+    $salary = trim($_POST["salary"]);
+}
+
+
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+    // Check input errors before inserting in database
+    if(empty($name_err) && empty($address_err) && empty($salary_err) && empty($username_err) && empty($password_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (name, address, salary, username, password) VALUES (?, ?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ssiss", $param_name, $param_address, $param_salary,$param_username,$param_password);
+            
+            // Set parameters
+            $param_name = $name;
+            $param_address = $address;
+            $param_salary = $salary;
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: login.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+	<link href="513p/style.css" rel="stylesheet" type="text/css" media="all" />
+	<link href="513p/default.css" rel="stylesheet" type="text/css" media="all" />
+</head>    
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title></title>
+<link href="default1.css" rel="stylesheet" type="text/css" media="all" />
+
+</head>
+
+
+<body>
+
+<!-- Body Wrapper -->
+<div id="body-wrapper" class="animsition">
+
+    <!-- Header -->
+    <header id="header" class="light">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <!-- Logo -->
+                    <div class="module module-logo dark">
+                        <a href="index.html">
+                            <img src="assets/img/photos/logo.jpg" alt="" width="300">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <!-- Navigation -->
+                    <nav class="module module-navigation left mr-4">
+                        <ul id="nav-main" class="nav nav-main">
+							<li><a href="index.html">Home</a></li>
+							<li><a href="page-about.html">About Us</a></li>
+							<li><a href="page-services.html">Services</a></li>
+							<li><a href="checkout.html">Checkout</a></li>
+							<li><a href="page-contact.html">Contact</a></li>
+                            <li><a href="register.php">register</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+    </header>
+    <!-- Header / End -->
+
+    <!-- Header -->
+    <header id="header-mobile" class="light">
+
+        <div class="module module-nav-toggle">
+            <a href="#" id="nav-toggle" data-toggle="panel-mobile"><span></span><span></span><span></span><span></span></a>
+        </div>    
+
+        <div class="module module-logo">
+            <a href="index.html">
+                <img src="assets/img/logo-horizontal-dark.svg" alt="">
+            </a>
+        </div>
+
+        <a href="#" class="module module-cart" data-toggle="panel-cart">
+            <i class="ti ti-shopping-cart"></i>
+            <span class="notification">2</span>
+        </a>
+
+    </header>
+    <!-- Header / End -->
+
+    <!-- Content -->
+    <div id="content">
+
+        <!-- Section - Main -->
+        <section class="section section-main section-main-1 bg-light">
+            
+            <div class="container dark">
+                <div class="slide-container">
+                    <div id="section-main-1-carousel-image" class="image inner-controls">
+                        <div class="slide"><div class="bg-image"><img src="assets/img/photos/7.jpg" alt=""></div></div>
+                        <div class="slide"><div class="bg-image"><img src="assets/img/photos/8.jpg" alt=""></div></div>
+                        <div class="slide"><div class="bg-image"><img src="assets/img/photos/9.jpg" alt=""></div></div>
+                    </div>
+                    <div class="content dark">
+                        <div id="section-main-1-carousel-content">
+                            <div class="content-inner">
+                                <h4 class="text-muted">Welcome</h4>
+                                <h1>Luca’s lovert</h1>
+                                <div class="btn-group">
+                                    <a href="#productModal" data-toggle="modal" class="btn btn-outline-primary btn-lg"><span>salad</span></a>
+                                    <span class="price price-lg">$10</span>
+                                </div>
+                            </div>
+                            <div class="content-inner">
+                                <h1>Welcome</h1>
+                                <h5 class="text-muted mb-5">Luca’s lover</h5>
+                                <a href="page-offers.html" class="btn btn-outline-primary btn-lg"><span>French fries</span></a>
+                            </div>
+                            <div class="content-inner">
+                                <h1>Welcome</h1>
+                                <h5 class="text-muted mb-5">Luca’s lover</h5>
+                                <a href="menu-list-collapse.html" class="btn btn-outline-primary btn-lg"><span>Fresh Fruit Platter</span></a>
+                            </div>
+                        </div>
+                        <nav class="content-nav">
+                            <a class="prev" href="#"><i class="ti-arrow-left"></i></a>
+                            <a class="next" href="#"><i class="ti-arrow-right"></i></a>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+
+        </section>
+
+        
+        <head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ width: 360px; padding: 20px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Address</label>
+                            <textarea name="address" class="form-control <?php echo (!empty($address_err)) ? 'is-invalid' : ''; ?>"><?php echo $address; ?></textarea>
+                            <span class="invalid-feedback"><?php echo $address_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Salary</label>
+                            <input type="text" name="salary" class="form-control <?php echo (!empty($salary_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $salary; ?>">
+                            <span class="invalid-feedback"><?php echo $salary_err;?></span>
+                        </div>
+            <div class="form-group">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <label>Confirm Password</label>
+                <input type="confirm_password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Submit">
+                <input type="reset" class="btn btn-secondary ml-2" value="Reset">
+            </div>
+            <p>Already have an account? <a href="login.php">Login here</a>.</p>
+        </form>
+    </div>    
+</body>
+
+        <!-- Footer -->
+        <footer id="footer" class="bg-dark dark">
+            
+            <div class="container">
+                <!-- Footer 1st Row -->
+                <div class="footer-first-row row">
+                    <div class="col-lg-3 text-center">
+                        <a href="index.html"><img src="assets/img/photos/logo.jpg" alt="" width="300" class="mt-5 mb-5"></a>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <h5 class="text-muted">Latest news</h5>
+                        <ul class="list-posts">
+                            <li>
+                                <a href="#" class="title">How to create effective webdeisign?</a>
+                                <span class="date">February 14, 2015</span>
+                            </li>
+                            <li>
+                                <a href="#" class="title">Awesome weekend in Polish mountains!</a>
+                                <span class="date">February 14, 2015</span>
+                            </li>
+                            <li>
+                                <a href="#" class="title">How to create effective webdeisign?</a>
+                                <span class="date">February 14, 2015</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-5 col-md-6">
+                        <h5 class="text-muted">Subscribe Us!</h5>
+                        <!-- MailChimp Form -->
+                        <form action="/suelo.us12.list-manage.com/subscribe/post-json?u=ed47dbfe167d906f2bc46a01b&amp;id=24ac8a22ad" id="sign-up-form" class="sign-up-form validate-form mb-3" method="POST">
+                            <div class="input-group">
+                                <input name="EMAIL" id="mce-EMAIL" type="email" class="form-control" placeholder="Tap your e-mail..." required>
+                                <span class="input-group-btn">
+                                    <button class="btn btn-primary btn-submit" type="submit">
+                                        <span class="description">Subscribe</span>
+                                        <span class="success">
+                                            <svg x="0px" y="0px" viewBox="0 0 32 32"><path stroke-dasharray="19.79 19.79" stroke-dashoffset="19.79" fill="none" stroke="#FFFFFF" stroke-width="2" stroke-linecap="square" stroke-miterlimit="10" d="M9,17l3.9,3.9c0.1,0.1,0.2,0.1,0.3,0L23,11"></svg>
+                                        </span>
+                                        <span class="error">Try again...</span>
+                                    </button>
+                                </span>
+                            </div>
+                        </form>
+                        <h5 class="text-muted mb-3">Social Media</h5>
+                        <a href="#" class="icon icon-social icon-circle icon-sm icon-facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="#" class="icon icon-social icon-circle icon-sm icon-google"><i class="fa fa-google"></i></a>
+                        <a href="#" class="icon icon-social icon-circle icon-sm icon-twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="#" class="icon icon-social icon-circle icon-sm icon-youtube"><i class="fa fa-youtube"></i></a>
+                        <a href="#" class="icon icon-social icon-circle icon-sm icon-instagram"><i class="fa fa-instagram"></i></a>
+                    </div>
+                </div>
+                <!-- Footer 2nd Row -->
+                <div class="footer-second-row">
+                    <span class="text-muted">Copyright &copy; 2017.Company name All rights reserved.<a target="_blank" href="http://sc.chinaz.com/moban/">&#x7F51;&#x9875;&#x6A21;&#x677F;</a></span>
+                </div>
+            </div>
+
+            <!-- Back To Top -->
+            <a href="#" id="back-to-top"><i class="ti ti-angle-up"></i></a>
+
+        </footer>
+        <!-- Footer / End -->
+
+    </div>
+    <!-- Content / End -->
+
+    <!-- Body Overlay -->
+    <div id="body-overlay"></div>
+
+</div>
+
+
+<!-- JS Plugins -->
+<script src="assets/plugins/jquery/dist/jquery.min.js"></script>
+<script src="assets/plugins/tether/dist/js/tether.min.js"></script>
+<script src="assets/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="assets/plugins/slick-carousel/slick/slick.min.js"></script>
+<script src="assets/plugins/jquery.appear/jquery.appear.js"></script>
+<script src="assets/plugins/jquery.scrollto/jquery.scrollTo.min.js"></script>
+<script src="assets/plugins/jquery.localscroll/jquery.localScroll.min.js"></script>
+<script src="assets/plugins/jquery-validation/dist/jquery.validate.min.js"></script>
+<script src="assets/plugins/jquery.mb.ytplayer/dist/jquery.mb.YTPlayer.min.js"></script>
+<script src="assets/plugins/twitter-fetcher/js/twitterFetcher_min.js"></script>
+<script src="assets/plugins/skrollr/dist/skrollr.min.js"></script>
+<script src="assets/plugins/animsition/dist/js/animsition.min.js"></script>
+
+<!-- JS Core -->
+<script src="assets/js/core.js"></script>
+
+<!-- JS Stylewsitcher -->
+<script src="styleswitcher/styleswitcher.js"></script>
+
+</body>
+
+</html>
+
+
